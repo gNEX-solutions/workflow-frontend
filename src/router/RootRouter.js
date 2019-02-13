f3e36df18f8bec7a1d76bb77ac86698681aee61e
Charinda <charinda04@gmail.com>
2019-02-13 16:00:17 +0530

    WIP: login screen

--- src/router/RootRouter.js
+++ src/router/RootRouter.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
-import Login from '../modules/login/pages/Login'
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Login from '../modules/login/pages/Login';
 
 const RootRouter = () => (
   <Switch>
@@ -8,10 +8,10 @@ const RootRouter = () => (
     {/* <Route path="/auth/callback" component={AuthCallback} /> */}
     {/* <PrivateRoute path="/" component={MainPage} /> */}
     {/* <Route exact path="/" component={ProductList} /> */}
-    <Route path='/login' component={Login} />
+    <Route path="/login" component={Login} />
     {/* <Route path="/signup" component={Signup} /> */}
-    <Route path='/not-found' render={() => <h1>Page Not Found</h1>} />
+    <Route path="/not-found" render={() => <h1>Page Not Found</h1>} />
   </Switch>
-)
+);
 
-export default RootRouter
+export default RootRouter;
