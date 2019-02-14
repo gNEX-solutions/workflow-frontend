e8296ae8e8b551b6edafa163a71082bc639624fe
dinith72 <jdinith72@gmail.com>
2019-02-14 23:26:07 +0530

    created the history section with the header

---
+++ src/modules/HistorySection/main.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import HeaderComponent from "../../shared/header/headerComponent";
+class HistorySection extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <header> history section </header>
+        <HeaderComponent />
+      </React.Fragment>
+    );
+  }
+}
+
+export default HistorySection;
--- src/router/RootRouter.js
+++ src/router/RootRouter.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import HomePage from "../modules/home/pages/HomePage";
+import HistorySection from "../modules/HistorySection/main";
 
 const RootRouter = () => (
   <Switch>
     <Route exact path="/" component={HomePage} />
     <Route path="/not-found" render={() => <h1>Page Not Found</h1>} />
+    <Route path="/history" component={HistorySection} />
   </Switch>
 );
 
