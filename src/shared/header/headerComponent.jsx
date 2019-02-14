f7aeca605384190d86e85009a25003d3f11283ba
dinith72 <jdinith72@gmail.com>
2019-02-14 23:15:32 +0530

    created the basic structure of the header compoent

---
+++ src/shared/header/headerComponent.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class HeaderComponent extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <h1> this header compoent </h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default HeaderComponent;
