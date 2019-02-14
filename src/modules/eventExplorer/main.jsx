87f9530aa1d2ea3926a30f1f63c20e61d13519f8
dinith72 <jdinith72@gmail.com>
2019-02-14 23:33:04 +0530

    created the event eplorer section

---
+++ src/modules/eventExplorer/main.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import HeaderComopnet from "../../shared/header/headerComponent";
+class EventExplorer extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <header> event explorere section </header>
+        <HeaderComopnet />
+      </React.Fragment>
+    );
+  }
+}
+
+export default EventExplorer;
--- src/router/RootRouter.js
+++ src/router/RootRouter.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import HomePage from "../modules/home/pages/HomePage";
 import HistorySection from "../modules/HistorySection/main";
+import EventExplorer from "../modules/eventExplorer/main";
 
 const RootRouter = () => (
   <Switch>
     <Route exact path="/" component={HomePage} />
     <Route path="/not-found" render={() => <h1>Page Not Found</h1>} />
     <Route path="/history" component={HistorySection} />
+    <Route path="/eventExp" component={EventExplorer} />
   </Switch>
 );
 
