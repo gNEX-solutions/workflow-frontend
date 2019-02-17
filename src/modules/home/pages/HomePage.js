dcf7cd47c84f5dec5fc9a7a0e10beb0c700e79f1
charinda <charinda04@gmail.com>
2019-02-17 12:23:50 +0530

    eslint file updates

--- src/modules/home/pages/HomePage.js
+++ src/modules/home/pages/HomePage.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class HomePage extends Component {
-  render () {
+  render() {
     return (
       <div>
-        <h1>Main Screen Placeholder</h1>
+        <h1> Main Screen Placeholder </h1>{' '}
       </div>
-    )
+    );
   }
 }
 
-export default HomePage
+export default HomePage;
--- src/router/RootRouter.js
+++ src/router/RootRouter.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import Login from '../modules/login/pages/Login';
-import HomePage from '../modules/home/pages/HomePage'
+import HomePage from '../modules/home/pages/HomePage';
 
 const RootRouter = () => (
   <Switch>
-    <Route exact path='/' component={HomePage} />
-    {/* <Route exact path={RouteTypes.login} component={AuthLogin} /> */}
-    {/* <Route path="/auth/callback" component={AuthCallback} /> */}
-    {/* <PrivateRoute path="/" component={MainPage} /> */}
-    <Route path="/login" component={Login} />
-    {/* <Route path="/signup" component={Signup} /> */}
-    <Route path="/not-found" render={() => <h1>Page Not Found</h1>} />
+    <Route exact path="/" component={HomePage} />{' '}
+    {/* <Route exact path={RouteTypes.login} component={AuthLogin} /> */}{' '}
+    {/* <Route path="/auth/callback" component={AuthCallback} /> */}{' '}
+    {/* <PrivateRoute path="/" component={MainPage} /> */}{' '}
+    <Route path="/login" component={Login} />{' '}
+    {/* <Route path="/signup" component={Signup} /> */}{' '}
+    <Route path="/not-found" render={() => <h1> Page Not Found </h1>} />
   </Switch>
 );
 
