7ecc1fb13a7378953852a428ac244dc5699d6e09
Pathmika Rajapakshe <kaveendrarajapakshe@gmail.com>
2019-02-21 20:53:45 +0530

    creating the iniital strcture

---
+++ src/shared/deleteDialogBox/deleteDialogBox.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+class DeleteDialogBox extends Component {
+  state = {};
+  render() {
+    return <p> delete dialog box </p>;
+  }
+}
+
+export default DeleteDialogBox;
