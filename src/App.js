ffa87991d074eb5fc51d6a56418c6930165c9bbe
LasithMadhawa <lasithmgunathilake@gmail.com>
2019-03-14 15:02:14 +0530

    User profile section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Router } from "react-router-dom";
 import createHistory from "history/createBrowserHistory";
 import { RootRouter } from "./router/index";
+
 // import logo from './logo.svg'
 import "./App.css";
 
