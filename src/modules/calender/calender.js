9fd9fc28e435eb7445575b18c28af6e37e9a1fb5
Dinith Jayabodhi <jdinith72@gmail.com>
2019-04-06 08:33:58 +0530

    Feature/calender section (#15)
    * install the moment.js
     can display dates programmatically
    
    * added the ability to show current date
    
    * added the year drop down compoennt
    
    * calender updates according to the
    month and year selected
    
    * added the fontawsome icons to the calendr
    
    * created the event info component
    
    * cpoying the table row styles to
    tableRow.css
    
    * made the current date visible only for the spec month
    and year
    
    * changing check circle to check
    
    * removing the warnings

--- src/modules/calender/calender.js
+++ src/modules/calender/calender.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
 
-import ApprovalSection from '../../shared/approval_section/approvalSectionComponent';
-import EventDescription from '../../shared/eventDescription/eventDescription';
-import CalenderFooter from '../../shared/calenderFooter/calenderFooter';
-import './calender.css';
+import React, { Component } from "react";
+import HeaderComponent from "../../shared/header/headerComponent";
+import MainMenuCompoannent from "../../shared/mainMenu/mainMenu";
+import ApprovalSection from "../../shared/approval_section/approvalSectionComponent";
+import { Row, Col } from "react-bootstrap";
+import EventDescription from "../../shared/eventDescription/eventDescription";
+import CalenderFooter from "../../shared/calenderFooter/calenderFooter";
+import CalenderComponent from "../../shared/calenderComponent/calenderComponent"
+import "./calender.css";
 
 class CalenderSection extends Component {
   state = {
@@ -53,7 +56,7 @@ class CalenderSection extends Component {
         </Row> */}
         <Row>
           <Col className="col-4" id="calender_section">
-            {/* <ApprovalSection></ApprovalSection> */}
+            <CalenderComponent />
           </Col>
           <Col className="col-8" id="event_info">
             <Row id="event_description_row">
