28aa57aec14a8543b4d93141ac399824c2bfdbdd
Laalitha Gajanayake <laalithagajanayake@gmail.com>
2019-04-11 22:10:35 +0530

    Styling Components

--- src/shared/mainMenu/mainMenu.js
+++ src/shared/mainMenu/mainMenu.js
@@ -1,103 +1,110 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Col, Button } from "react-bootstrap";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlusCircle, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusCircle, faSearch } from "@fortawesome/free-solid-svg-icons";
 
-import './mainMenu.css';
+import "./mainMenu.css";
 
 class MainMenuComponent extends Component {
   state = {
-    eventExpStatus: 'active',
-    calenderStatus: '',
-    historyStatus: ''
+    eventExpStatus: "active",
+    calenderStatus: "",
+    historyStatus: ""
   };
 
   eventExpClicked() {
     // alert('event exp clicked');
-    console.log('event exp clicked ');
+    console.log("event exp clicked ");
   }
 
   calenderCicked() {
-    console.log('calender clicked');
+    console.log("calender clicked");
     this.setState({
-      eventExpStatus: '',
-      calenderStatus: 'active',
-      historyStatus: ''
-
-    })
+      eventExpStatus: "",
+      calenderStatus: "active",
+      historyStatus: ""
+    });
   }
 
   searchButtonClicked() {
     // alert('search btn clicked');
-    console.log('search btn clicked');
+    console.log("search btn clicked");
   }
   render() {
     return (
       <React.Fragment>
-
-
-        <Col className="col-1">
-
-        </Col>
+        <Col className="col-1" />
 
         <Col className="col-2">
-          <Button variant="success" id="add_button" >
-
-            <FontAwesomeIcon icon={faPlusCircle} size="2x" />
-
-            &nbsp; &nbsp;  New Event
-
-
-
+          <Button variant="success" id="add_button">
+            <FontAwesomeIcon icon={faPlusCircle} size="1x" />
+            &nbsp; Add New Event
           </Button>
-        </Col >
+        </Col>
         <Col className="col-2" id="calender">
           <div className={this.state.calenderStatus}>
-            <Link to={"/calender"} onClick={this.calenderCicked} >
+            <Link to={"/calender"} onClick={this.calenderCicked}>
               <h3>
-                <span className="badge badge-light" id={this.state.calenderStatus}>Event Calender</span>
+                <span
+                  className="badge badge-light"
+                  id={this.state.calenderStatus}
+                >
+                  Event Calender
+                </span>
               </h3>
             </Link>
           </div>
-
         </Col>
         <Col className="col-2" id="explorer">
           <div id={this.state.eventExpStatus}>
             <Link to={"/eventExp"} onClick={this.eventExpClicked}>
               <h3>
-                <span className="badge badge-light" id={this.state.eventExpStatus}>Event Explorer</span>
+                <span
+                  className="badge badge-light"
+                  id={this.state.eventExpStatus}
+                >
+                  Event Explorer
+                </span>
               </h3>
             </Link>
-
           </div>
         </Col>
         <Col className="col-2" id="history">
           <div className={this.state.historyStatus}>
-            <Link to={"/history"} >
+            <Link to={"/history"}>
               <h3>
-                <span className="badge badge-light" id="active">History</span>
+                <span className="badge badge-light" id="active">
+                  History
+                </span>
               </h3>
             </Link>
           </div>
         </Col>
         <Col className="col-2" id="search_box">
           <div className="input-group mb-3" id="search_group">
-            <input type="text" className="form-control" placeholder="search events" aria-label="Recipient's username" aria-describedby="button-addon2" onKeyPress={this.searchButtonClicked} />
+            <input
+              type="text"
+              className="form-control"
+              placeholder="search events"
+              aria-label="Recipient's username"
+              aria-describedby="button-addon2"
+              onKeyPress={this.searchButtonClicked}
+            />
 
-            <div className="input-group-append" >
-              <button className="btn btn-outline-secondary" type="button" id="button-search" onClick={this.searchButtonClicked} >
+            <div className="input-group-append">
+              <button
+                className="btn btn-outline-secondary"
+                type="button"
+                id="button-search"
+                onClick={this.searchButtonClicked}
+              >
                 <FontAwesomeIcon icon={faSearch} />
               </button>
             </div>
           </div>
-
         </Col>
-
-
-
-
-      </React.Fragment >
+      </React.Fragment>
     );
   }
 }
