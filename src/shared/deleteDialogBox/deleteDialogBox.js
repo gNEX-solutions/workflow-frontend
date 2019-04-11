efb04906b21fd8173cdb6246430faf12a5471a47
Pathmika Rajapakshe <kaveendrarajapakshe@gmail.com>
2019-04-11 13:01:27 +0530

    Delete dialog box code with changes

--- src/shared/deleteDialogBox/deleteDialogBox.js
+++ src/shared/deleteDialogBox/deleteDialogBox.js
@@ -1,8 +1,56 @@
 import React, { Component } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./deleteDialogBoX.css";
+
 class DeleteDialogBox extends Component {
-  state = {};
+  state = {
+    eventName: "CSR Project",
+    eventDate: "26th January",
+    eventTime: "9 am",
+    eventVenue: "Apeksha Hospital"
+  };
+
+  deleteEvent = () => {};
   render() {
-    return <p> delete dialog box </p>;
+    return (
+      <React.Fragment>
+        <Card className="text-center">
+          <Card.header>Delete Event</Card.header>
+          <Card.Body>
+            <Card.text>
+              <h6>Are you sure you want to delete this event</h6>
+              <Row>
+                <Col>
+                  <h6>{this.state.eventName}</h6>
+                  <h6>{this.state.eventDate}</h6>
+                  <h6>
+                    {this.state.eventTime}
+                    <p> onwards</p>
+                  </h6>
+                  <h6>@{this.state.eventVenue}</h6>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Button variant="primary">No</Button>
+
+                  <Button
+                    onClick={this.deleteEvent}
+                    variant="warning"
+                    style={{ marginLeft: 5 }}
+                  >
+                    Yes
+                  </Button>
+                </Col>
+              </Row>
+            </Card.text>
+          </Card.Body>
+        </Card>
+      </React.Fragment>
+    );
   }
 }
 
