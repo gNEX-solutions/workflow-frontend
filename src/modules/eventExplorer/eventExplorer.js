26d9d041046e8a14848145acb6ef3cae7b4186f0
dinith72 <jdinith72@gmail.com>
2019-04-17 23:37:16 +0530

    added the basic table structure for the eent explorer

--- src/modules/eventExplorer/eventExplorer.js
+++ src/modules/eventExplorer/eventExplorer.js
@@ -1,14 +1,41 @@
 import React, { Component } from "react";
 import HeaderComopnet from "../../shared/header/headerComponent";
 import MainMenuCompoannent from "../../shared/mainMenu/mainMenu";
+import { Row, Col, Table } from "react-bootstrap";
+import './eventExplorer.css';
 class EventExplorer extends Component {
   state = {};
   render() {
     return (
       <React.Fragment>
-        <header> event explorere section </header>
-        <HeaderComopnet />
-        <MainMenuCompoannent />
+        <Row>
+          <HeaderComopnet />
+        </Row>
+        <Row>
+          <MainMenuCompoannent />
+        </Row>
+
+        <Row>
+          <div className="table-responsive">
+            <Col className="col-md-10 col-sm-10">
+              <Table striped bordered responsive>
+                <thead>
+                  <tr>
+                    <th> &nbsp; &nbsp;</th>
+                    <th>1st Year</th>
+                    <th>2nd Year</th>
+                    <th>3rd Year</th>
+                    <th>4th Year</th>
+                  </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+
+              </Table>
+            </Col>
+          </div>
+        </Row>
       </React.Fragment>
     );
   }
