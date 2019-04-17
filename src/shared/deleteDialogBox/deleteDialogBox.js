71c8f4649dc1a583dfd0831df32ed0721b1b2796
Pathmika Rajapakshe <kaveendrarajapakshe@gmail.com>
2019-04-18 00:38:08 +0530

    Changes to the component

--- src/shared/deleteDialogBox/deleteDialogBox.js
+++ src/shared/deleteDialogBox/deleteDialogBox.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faExclamationTriangle,
+  faCalendarTimes
+} from "@fortawesome/free-solid-svg-icons";
+
 import "./deleteDialogBoX.css";
 
 class DeleteDialogBox extends Component {
@@ -17,36 +25,61 @@ class DeleteDialogBox extends Component {
   render() {
     return (
       <React.Fragment>
-        <Card className="text-center">
-          <Card.header>Delete Event</Card.header>
+        <Card
+          className="text-center"
+          style={{ width: "20rem", margin: "auto", marginTop: "10px" }}
+        >
+          <Card.Header style={{ backgroundColor: "black", color: "white" }}>
+            DELETE EVENT
+          </Card.Header>
           <Card.Body>
-            <Card.text>
-              <h6>Are you sure you want to delete this event</h6>
+            <Card.Text>
               <Row>
                 <Col>
-                  <h6>{this.state.eventName}</h6>
-                  <h6>{this.state.eventDate}</h6>
                   <h6>
-                    {this.state.eventTime}
-                    <p> onwards</p>
+                    <FontAwesomeIcon
+                      icon={faExclamationTriangle}
+                      className="single_tick"
+                    />
+                    <span> </span> Are you sure you want to delete this event
                   </h6>
-                  <h6>@{this.state.eventVenue}</h6>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs lg="3">
+                  <FontAwesomeIcon
+                    size="4x"
+                    icon={faCalendarTimes}
+                    className="far fa-calendar-times"
+                  />
+                </Col>
+                <Col md="auto">
+                  <div>
+                    <h6>{this.state.eventName}</h6>
+                    <h6>{this.state.eventDate}</h6>
+                    <h6>
+                      {this.state.eventTime}
+                      <span> </span>
+                      onwards
+                    </h6>
+                    <h6>@{this.state.eventVenue}</h6>
+                  </div>
                 </Col>
               </Row>
               <Row>
                 <Col>
-                  <Button variant="primary">No</Button>
+                  <Button variant="outline-primary">No</Button>
 
                   <Button
                     onClick={this.deleteEvent}
-                    variant="warning"
+                    variant="outline-primary"
                     style={{ marginLeft: 5 }}
                   >
                     Yes
                   </Button>
                 </Col>
               </Row>
-            </Card.text>
+            </Card.Text>
           </Card.Body>
         </Card>
       </React.Fragment>
