91f3188b820ab42b5c232b048c51294d596d00cc
dinith72 <jdinith72@gmail.com>
2019-04-19 23:56:52 +0530

    add onclick method to each of the compoent of event info

--- src/modules/eventExplorer/monthEventsComponet.js
+++ src/modules/eventExplorer/monthEventsComponet.js
@@ -1,13 +1,21 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable prettier/prettier */
 /* eslint-disable indent */
-/* wifi - D7AAF404  */
 import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
 
 import { Badge } from "react-bootstrap";
 import './monthEventcomponents.css';
 
+
+
 class MonthEventCompoent extends Component {
     state = {};
+    // propType = {
+    //     month: PropTypes.number,
+    //     year: PropTypes.number
+
+    // }
     eventsArray = [
         {
             eventId: "1",
@@ -22,7 +30,7 @@ class MonthEventCompoent extends Component {
             eventOrganizers: "4"
         },
         {
-            eventId: "2",
+            eventId: "211",
             eventName: "im piritha",
             eventDate: new Date('2019-3-13'),
             eventOrganizers: "3"
@@ -42,17 +50,28 @@ class MonthEventCompoent extends Component {
 
         }
     ]
-
+    onEventClicked(event) {
+        // alert('comp clicked');
+        console.log(event.target.id);
+    }
 
     getFilteredEventList() {
-        let { month, level } = this.props;
+        const { month, level } = this.props;
         const filteredList = this.eventsArray.filter(({ eventDate, eventOrganizers } = {}) => {
             return eventDate.getMonth() === month && level == eventOrganizers;
         }).map(({ eventId, eventName } = {}) => {
-            return (<h3> <Badge variant="primary" key={eventId} id="eventInto_badge"> {eventName}  </Badge></h3>);
+            return (
+                <h3 key={eventId}>
+                    <Badge pill variant="info" onClick={this.onEventClicked}
+                        key={eventId} id={eventId} className="eventInto_badge">
+                        {eventName}
+                    </Badge>
+                </h3>);
         });
         return filteredList;
     }
+
+
 
     render() {
         this.getFilteredEventList();
