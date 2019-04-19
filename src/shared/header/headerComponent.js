897c62bc13c348393528882f3ba0212b613987d0
Charinda Dissanayake <charinda04@gmail.com>
2019-04-20 05:04:02 +0530

    Revert "replace material UI with reactstrap (#18)"
    This reverts commit fc347511e491777f6512c78122f894010a00f97d.

--- src/shared/header/headerComponent.js
+++ src/shared/header/headerComponent.js
@@ -1,67 +1,14 @@
-
-import React from "react";
-import { Collapse, Button, CardBody, CardHeader, Card } from "reactstrap";
-import "bootstrap/dist/css/bootstrap.css";
-
-let TodayDay = "Today";
-let NextWeek = "Next Week";
-let NextMonth = "Next Month";
-
-let TodayNotification = 0;
-let NextWeekNotification = 0;
-let NextMonthNotification = 0;
-
-class NotificationBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.toggle = this.toggle.bind(this);
-    this.state = { collapse: false };
-  }
-
-  toggle() {
-    this.setState(state => ({ collapse: !state.collapse }));
-  }
-
+import React, { Component } from "react";
 
 class HeaderComponent extends Component {
   state = {};
   render() {
     return (
-
-      <div>
-        <Button
-          color="primary"
-          onClick={this.toggle}
-          style={{ marginBottom: "1rem" }}
-        >
-          Notifications
-        </Button>
-        <Collapse isOpen={this.state.collapse}>
-          <Card>
-            <CardHeader>
-              {TodayDay} Events ({TodayNotification})
-            </CardHeader>
-            <CardBody>Event</CardBody>
-          </Card>
-          <Card>
-            <CardHeader>
-              {NextWeek} Events ({NextWeekNotification})
-            </CardHeader>
-            <CardBody>Event</CardBody>
-          </Card>
-          <Card>
-            <CardHeader>
-              {NextMonth} Events ({NextMonthNotification})
-            </CardHeader>
-            <CardBody>Event</CardBody>
-          </Card>
-        </Collapse>
-      </div>
-
+      <React.Fragment>
+        <h1> this header compoent </h1>
+      </React.Fragment>
     );
   }
 }
 
-
-export default NotificationBar;
-
+export default HeaderComponent;
