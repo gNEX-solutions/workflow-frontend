60262f435c9c1de1e9ea33069f9222d6c8ce7aab
sandalu95 <sandalukalpanee@gmail.com>
2019-05-01 16:20:37 +0530

    comment section added

---
+++ src/shared/commentSection/Comments.jsx
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import 'bootstrap/dist/css/bootstrap.css';
+import Comment from './Comment';
+import './Comments.css';
+
+/**
+ * Display the comment list
+ * @class Comments
+ * @extends {React.Component}
+ */
+class Comments extends Component {
+    /**
+     * Creates an instance of Comments
+     * @param {*} props properies passed by the parent element
+     * @memberof Comments
+     */
+    constructor(props) {
+        super(props);
+        this.state = {
+            comments: [],
+        };
+        this.getAllComments = this.getAllComments.bind(this);
+    }
+
+    /**
+     * Gets all the comments for a particular event, when component mounts
+     * @memberof Comments
+     */
+    componentDidMount() {
+        const { event } = this.props;
+        const comments = this.getAllComments(event.id);
+        if(comments){
+            this.setState({ comments });
+        } else {
+            console.log("Something went wrong while retrieving the comments");
+        }
+    }
+
+    /**
+     * Receive all the comments for the active event
+     * @param {eventId} props Id of the active event
+     * @memberof Comments
+     */
+    getAllComments(eventId) {
+        const comments = [
+            {
+                commentId:"c001",
+                commentText:"Can you please change the venue?",
+                createdBy:"Dilani Wickramarachchi",
+                date:new Date('2019-2-12'),
+            },
+            {
+                commentId:"c002",
+                commentText:"Can you please change the date?",
+                createdBy:"Ruwan Wickramarachchi",
+                date:new Date('2019-2-15'),
+            },
+            {
+                commentId:"c003",
+                commentText:"Can you please change the time?",
+                createdBy:"Chathura Rajapakse",
+                date:new Date('2019-2-18'),
+            },
+        ];
+        return comments;
+    }
+
+    /**
+     * Render method of the component
+     * @returns {React.Component} Comment html component
+     * @memberof Comments
+     */
+    render() {
+        const { event } = this.props;
+        const { comments } = this.state;
+        return (
+            <React.Fragment>
+                <div className="commentSection">
+                    <div className="heading-block">
+                        <h4>Conversation</h4>
+                    </div>
+                    <div className="container comments-display">
+                        <Comment comments={comments} event={event} />
+                    </div>
+                    <div className="row comment-area-block">
+                        <div className="col-sm-10 form-group basic-textarea rounded-corners comment-text-block">
+                            <textarea className="form-control z-depth-1" id="comment-text-area" rows="1" placeholder="Write your comment..."/>
+                        </div>
+                        <button type="button" className="col-sm-2 btn btn-light sendbtn">Send</button>
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+Comments.defaultProps = {
+    event: {id:"e001",title:"IM_Cricket_match"},
+};
+
+Comments.propTypes = {
+    event: PropTypes.instanceOf(Object),
+};
+
+export default Comments;
