f77011d666b4e03a537a650fe358c4047b7d9391
dinith72 <jdinith72@gmail.com>
2019-05-28 14:44:07 +0530

    travis build failed  bug fix

--- src/modules/eventExplorer/eventExplorer.js
+++ src/modules/eventExplorer/eventExplorer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Row, Col, Table } from 'react-bootstrap';
-import * as moment from 'moment';
+import moment from 'moment';
 import { connect } from 'react-redux';
 // import HeaderComopnet from '../../shared/header/headerComponent';
 // import MainMenuCompoannent from '../../shared/mainMenu/mainMenu';
